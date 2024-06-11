3dd8ecf789087b7f399159ab6b1e226a545da0cf
Eva <as45986@gmail.com>
2024-06-11 19:24:49 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <v-date-input label="Select a date" prepend-icon="" prepend-inner-icon="$calendar" variant="solo"></v-date-input>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
