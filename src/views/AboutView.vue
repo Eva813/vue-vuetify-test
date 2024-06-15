f0a044b4ea8c31ab3fc8369b7f185285b5b1703c
Eva <as45986@gmail.com>
2024-06-15 13:54:08 +0800

    add husky

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,9 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
     <v-date-input label="Select a date" prepend-icon="" prepend-inner-icon="$calendar" variant="solo"></v-date-input>
+    <div>dwedwed</div>
   </div>
 </template>
+<script setup lang="ts">
+const msg = 'Hello Vite + Vue 3 + TypeScript + VitePress!'
+const foo = () => {
+  console.log('foo')
 
+
+
+  return 'foo'
+}
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
